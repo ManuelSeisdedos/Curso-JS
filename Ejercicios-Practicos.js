1481c2f49823640f20ed8e8f92450440d0cdb948
ManuelSeisdedos <seisdedosmanuel2@gmail.com>
2022-11-30 11:29:14 -0300

    Segundos Ejercicios

--- Ejercicios-Practicos.js
+++ Ejercicios-Practicos.js
@@ -24,3 +24,50 @@ let NumCaracteresCortado = (texto, numero) => {
     return texto.slice(0, numero)
 }
 
+/* 3) Programa una función que dada una String te devuelva un 
+      Array de textos separados por cierto caracter, 
+      Por ejemplo: miFuncion('hola que tal', ' ') devolverá ['hola', 'que', 'tal'].*/ 
+
+let ArrayDeCaracteres = (texto, caracter) => {
+    if (typeof texto !== "string") return "El primer parámetro debe ser de tipo string."
+
+    return texto.split(caracter)
+}
+
+/* 4) Programa una función que repita un texto X veces, 
+      Por ejemplo: miFuncion('Hola Mundo', 3) devolverá Hola Mundo Hola Mundo Hola Mundo. */ 
+
+let CadenaRepetida = (texto, numero) => {
+    if (typeof texto !== "string") return "El primer parámetro debe ser de tipo string."
+    if (typeof numero !== "number") return "El segundo parámetro debe ser de tipo number"
+
+    return texto.repeat(numero)
+}
+
+/* 5) Programa una función que invierta las palabras de 
+      una cadena de texto, 
+      Por Ejemplo: miFuncion("Hola Mundo") devolverá "odnuM aloH". */
+
+let TextoInvertido = (texto) => {
+    if (typeof texto !== "string") return "El primer parámetro debe ser de tipo string."
+    
+    return texto.split("").reverse().join("")
+
+        
+}
+
+/* 6) Programa una función para contar el número de veces 
+      que se repite una palabra en un texto largo, 
+      Por ejemplo: miFuncion("hola mundo adios mundo", "mundo") devolverá 2. */
+
+let CuantasVeces = (texto, palabra) => {
+    if (typeof texto !== "string") return "El primer parámetro debe ser de tipo string."
+    if (typeof palabra !== "string") return "El primer parámetro debe ser de tipo string."
+    let arrayDeTexto = texto.split(" ")
+    
+    return arrayDeTexto.reduce((acc,curr) => {
+    if (curr === palabra) acc++
+      return acc
+      }, 0)
+    }
+    
