2cea4f635752d409c9501063f05b9b0f32151b0d
ManuelSeisdedos <seisdedosmanuel2@gmail.com>
2022-12-01 20:12:23 -0300

    Ejercicios Pt.3

--- Ejercicios-Practicos.js
+++ Ejercicios-Practicos.js
@@ -6,7 +6,7 @@
       caracteres de una cadena de texto,
       Por ejemplo: miFuncion("Hola Mundo") devolverá 10. */
 
-let NumCaracteres = (texto) => {
+let numCaracteres = (texto) => {
    
     if (typeof texto !== "string") return "El texto ingresado debe ser un string."
 
@@ -17,7 +17,7 @@ let NumCaracteres = (texto) => {
       según el número de caracteres indicados, 
       Por ejemplo: miFuncion("Hola Mundo", 4) devolverá "Hola".*/ 
 
-let NumCaracteresCortado = (texto, numero) => {
+let numCaracteresCortado = (texto, numero) => {
     if (typeof texto !== "string") return "El primer parámetro debe ser de tipo string."
     if (typeof numero !== "number") return "El segundo parámetro debe ser de tipo number"
 
@@ -28,7 +28,7 @@ let NumCaracteresCortado = (texto, numero) => {
       Array de textos separados por cierto caracter, 
       Por ejemplo: miFuncion('hola que tal', ' ') devolverá ['hola', 'que', 'tal'].*/ 
 
-let ArrayDeCaracteres = (texto, caracter) => {
+let arrayDeCaracteres = (texto, caracter) => {
     if (typeof texto !== "string") return "El primer parámetro debe ser de tipo string."
 
     return texto.split(caracter)
@@ -37,7 +37,7 @@ let ArrayDeCaracteres = (texto, caracter) => {
 /* 4) Programa una función que repita un texto X veces, 
       Por ejemplo: miFuncion('Hola Mundo', 3) devolverá Hola Mundo Hola Mundo Hola Mundo. */ 
 
-let CadenaRepetida = (texto, numero) => {
+let cadenaRepetida = (texto, numero) => {
     if (typeof texto !== "string") return "El primer parámetro debe ser de tipo string."
     if (typeof numero !== "number") return "El segundo parámetro debe ser de tipo number"
 
@@ -48,7 +48,7 @@ let CadenaRepetida = (texto, numero) => {
       una cadena de texto, 
       Por Ejemplo: miFuncion("Hola Mundo") devolverá "odnuM aloH". */
 
-let TextoInvertido = (texto) => {
+let textoInvertido = (texto) => {
     if (typeof texto !== "string") return "El primer parámetro debe ser de tipo string."
     
     return texto.split("").reverse().join("")
@@ -60,9 +60,10 @@ let TextoInvertido = (texto) => {
       que se repite una palabra en un texto largo, 
       Por ejemplo: miFuncion("hola mundo adios mundo", "mundo") devolverá 2. */
 
-let CuantasVeces = (texto, palabra) => {
+let cuantasVeces = (texto, palabra) => {
     if (typeof texto !== "string") return "El primer parámetro debe ser de tipo string."
     if (typeof palabra !== "string") return "El primer parámetro debe ser de tipo string."
+
     let arrayDeTexto = texto.split(" ")
     
     return arrayDeTexto.reduce((acc,curr) => {
@@ -71,3 +72,52 @@ let CuantasVeces = (texto, palabra) => {
       }, 0)
     }
     
+/* 7) Programa una función que valide si una palabra o 
+      frase dada, es un palíndromo (que se lee igual en un sentido que en otro), 
+      Por Ejemplo: mifuncion("Salas") devolverá true. */
+
+let funcionPalindromo = (texto) => {
+    if (typeof texto !== "string") return "El primer parámetro debe ser de tipo string."
+
+    let palindromo = texto.split("").reverse().join("")
+    return texto.toLowerCase() === palindromo.toLowerCase() ? true : false
+}
+
+/* 8) Programa una función que elimine cierto patrón 
+      de caracteres de un texto dado, 
+      Por Ejemplo: miFuncion("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz") devolverá  "1, 2, 3, 4 y 5. */
+
+let suprimir = (texto, sup) => {
+    if (typeof texto !== "string") return "El primer parámetro debe ser de tipo string."
+    if (typeof sup !== "string") return "El primer parámetro debe ser de tipo string."
+   
+    return texto.split(sup).join("")
+}
+
+/* 9) Programa una función que obtenga un numero aleatorio entre 501 y 600. */
+
+let randomNumber = () => {
+    let numero = Math.random()*100+500
+   
+    if (numero === 500) {
+        return 600
+    } else {
+        return Math.trunc(numero)
+    }
+}
+/* 10) Programa una función que reciba un número y evalúe si es capicúa o no 
+       (que se lee igual en un sentido que en otro), 
+       Por Ejemplo: miFuncion(2002) devolverá true. */
+
+let capicua = (numero) => {
+    if (typeof numero !== "number") return "El primer parámetro debe ser de tipo number."
+
+    let numerocapicua = numero.toString().split("").reverse().join("")
+
+    return Number(numerocapicua) === numero ?  true : false
+}
+
+
+/* 11) Programa una función que calcule el factorial de un número
+      (El factorial de un entero positivo n, se define como el producto de todos 
+      los números enteros positivos desde 1 hasta n), pe. miFuncion(5) devolverá 120.*/
